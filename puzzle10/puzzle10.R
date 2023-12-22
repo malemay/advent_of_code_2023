8ab9ad0e992c66361128fa5bf9f4a93274ca66f3
Marc-André Lemay <marc-andre.lemay.2@ulaval.ca>
2023-12-22 10:13:22 -0500

    Remove non-working ray tracing code from puzzle 10 solution

--- puzzle10/puzzle10.R
+++ puzzle10/puzzle10.R
@@ -2,7 +2,6 @@
 
 # Read the input and format as a matrix
 x <- readLines("puzzle10.txt")
-#x <- readLines("test.txt")
 x <- do.call("rbind", strsplit(x, ""))
 
 # Find the position of the "S"
@@ -44,7 +43,6 @@ next_move <- function(move, pipe_matrix) {
 # until we come back to S
 current_pos <- c(s_row, s_col)
 current_move <- c(0, 1)
-#current_move <- c(1, 0)
 current_pos <- current_pos + current_move
 n_moves <- 1
 
@@ -68,12 +66,10 @@ cat("PUZZLE 10A:", n_moves / 2, "\n")
 # We know that our polygon has 14024 vertices
 # so we need a matrix with one more row
 vertices <- matrix(nrow = 14025, ncol = 2)
-#vertices <- matrix(nrow = 161, ncol = 2)
 vertices[1, ] <- c(s_row, s_col)
 
 current_pos <- c(s_row, s_col)
 current_move <- c(0, 1)
-#current_move <- c(1, 0)
 current_pos <- current_pos + current_move
 n_moves <- 1
 vertices[n_moves + 1, ] <- current_pos
@@ -96,113 +92,15 @@ in_loop <- matrix(FALSE, nrow = nrow(x), ncol = ncol(x))
 in_loop[vertices] <- TRUE
 
 x[s_row, s_col] <- "-"
-#x[s_row, s_col] <- "7"
 vertices <- vertices[!x[vertices] %in% c("-", "|"), ]
-
-# A function that computes the linear equation of a line
-# from a pair of points
-line_equation <- function(pos1, pos2) {
-	# Extracting the x-y coordinates for clarity
-	x1 <- pos1[1]
-	y1 <- pos1[2]
-	x2 <- pos2[1]
-	y2 <- pos2[2]
-
-	# Getting the parameters of the equation from these
-	a <- y2 - y1
-	b <- x1 - x2
-	c <- (x2 * y1) - (x1 * y2)
-
-	c(a, b, c)
-}
 
 # Creating a matrix for all sides of the polygon
 sides <- cbind(vertices[1:(nrow(vertices) - 1),], vertices[2:nrow(vertices), ])
 sides <- sides[, c(2, 1, 4, 3)]
 colnames(sides) <- c("x1", "y1", "x2", "y2")
 
-# Precomputing the linear equation parameters for each polygon side
-params <- matrix(nrow = nrow(sides), ncol = 3)
-
-for(i in 1:nrow(params)) params[i, ] <- line_equation(sides[i, 1:2], sides[i, 3:4])
-
-# A function that tests whether a point is in the polygon
-point_in_poly <- function(point, sides, side_params) {
-	# We use c(-pi,-pi) as the first point in the line to test
-	# The reason for that is that sometimes the line would go
-	# through a vertex and thus result in two crosses instead
-	# of one. By choosing irrational numbers I am hoping
-	# for such issues to be less frequent
-
-	# The side_params matrix gives us the parameters
-	# describing the lines passing through each polygon side
-	# We need to compute on what side of that line the (0, 0)
-	# point is. This is given to us by the C parameter because
-	# x and y are both 0
-	# Therefore the other computation left is the position
-	# of the point itself relative to the line
-	d1 <- side_params[, 3]
-	d2 <- side_params[, 1] * point[1] + side_params[, 2] * point[2] + side_params[, 3]
-
-	# There is for sure no intersection if both are the same sign
-	no_intersection <- (d1 > 0 & d2 > 0) | (d1 < 0 & d2 < 0)
-
-	# We can use that to subset the sides to analyse
-	sides <- sides[!no_intersection, , drop = FALSE]
-	if(nrow(sides) == 0) return(FALSE)
-
-	# Now we do things the other way around by computing
-	# line equation parameters for the line going through
-	# (0, 0) and the point of interest
-	x1 <- 0
-	y1 <- 0
-	x2 <- point[1]
-	y2 <- point[2]
-
-	a <- y2 - y1
-	b <- x1 - x2
-	c <- (x2 * y1) - (x1 * y2)
-
-	# Computing the sign relative to that line for each
-	# of the two points describing each side
-	s1 <- a * sides[, 1] + b * sides[, 2] + c
-	s2 <- a * sides[, 3] + b * sides[, 4] + c
-	
-	# Removing the sides for which there is no intersection
-	no_intersection <- (s1 > 0 & s2 > 0) | (s1 < 0 & s2 < 0)
-	sides <- sides[!no_intersection, , drop = FALSE]
-
-	# The point is within the polygon if there is an odd
-	# number of intersections
-	n_intersections <- nrow(sides)
-
-	# If there are points directly on the line then we need
-	# to substract because they will be counted twice
-	#twice_vertices <- rbind(sides[a * sides[, 1] + b * sides[, 2] + c == 0, c(1, 2)],
-	#			sides[a * sides[, 3] + b * sides[, 4] + c == 0, c(3, 4)])
-	#twice_vertices <- unique(twice_vertices)
-	#n_intersections <- n_intersections - nrow(twice_vertices)
-	n_intersections %% 2 != 0
-}
-
-# Now we need to test every point that is not in the loop
-n_within <- 0
-within_matrix <- matrix(FALSE, nrow = nrow(x), ncol = ncol(x))
-
-for(i in 1:nrow(x)) {
-	for(j in 1:ncol(x)) {
-		if(in_loop[i, j]) next
-		if(point_in_poly(c(j, i), sides, params)) {
-		n_within <- n_within + 1
-		within_matrix[i, j] <- TRUE
-		}
-	}
-}
-
-cat("Puzzle 10B:", n_within, "\n")
-
-# Trying another solution involving the winding number algorithm
-# IT involves finding the oriented angle between points and each side of
+# Trying solution involving the winding number algorithm
+# It involves finding the oriented angle between points and each side of
 # the polygon, and summing those angles. The angle should be 360 degrees
 # if the point is within the polygon, and 0 otherwise
 
@@ -220,10 +118,12 @@ angle_sum <- function(point, sides) {
 	sum(angle(x1, y1, x2, y2))
 }
 
+# This matrix holds the values for all points
 angle_sums <- matrix(0, nrow = nrow(x), ncol = ncol(x))
 
 for(i in 1:nrow(x)) {
 	for(j in 1:ncol(x)) {
+		# We do not need to test the points that are part of the loop
 		if(in_loop[i, j]) next
 		angle_sums[i, j] <- angle_sum(c(j, i), sides)
 	}
