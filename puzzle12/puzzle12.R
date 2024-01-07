90ac95159a68b8d17b8b53f88bed3bac16e88b55
Marc-André Lemay <marc-andre.lemay.2@ulaval.ca>
2024-01-07 12:34:55 -0500

    Update R code version for puzzle 12; still not fast enough, would need to implement a cache as in the C++ version

--- puzzle12/puzzle12.R
+++ puzzle12/puzzle12.R
@@ -48,12 +48,35 @@ cat("Puzzle 12A:", sum(unlist(n_possibilities)), "\n")
 
 # PUZZLE 12B
 
+# A function that preprocesses a sequence to determine the maximum number of hashes
+# that can fit for each index
+preprocess_sequence <- function(x) {
+	output <- numeric(length(x))
+
+	for(i in 1:length(x)) {
+		j <- i
+		while(x[j] != "." && j <= length(x)) {
+			output[i] <- output[i] + 1
+			j <- j + 1
+		}
+	}
+
+	output
+}
+
+# A function that returns a logical vector of positions of the hashes in a sequence
+# The length is 1 more than the length of the sequence because we need to be able
+# to tell whether there is a hash after a sequence
+hash_positions <- function(x) {
+	c(x == "#", FALSE)
+}
+
 # A function that computes the number of combinations to test
 # given a number of '#' symbols to reach, the number already there,
 # and the number of '?' symbols
 
 # A recursive function that tests a target series of '#' symbols against a sequence
-test_sequence <- function(sequence, series) {
+test_sequence <- function(sequence, seqlength, pp_vect, hpos, which_h, index, series) {
 
 	# Initialize the return value to 0
 	return_value <- 0
@@ -61,64 +84,84 @@ test_sequence <- function(sequence, series) {
 	# If this is the last element of the sequence then we can increment the return value
 	# by the number of times that the series sites into the remaining sequence
 	# But we must make sure that the admissible sequence is just one stretch
-	#if(length(series) == 1) {
-	if(length(series) == 1) {
-		# We test if the sequence is compatible with the provided series of numbers
-		# We must make sure that we have consumed all the '#' symbols in the sequence
-		# If the sequence does not work, we cut the first symbol so we can try the next one
-		# We can move it along to the end and count the number of matches
-		# However we must make sure to consume all # symbols along the way
-
-		while(series <= length(sequence)) {
-			if(all(sequence[1:series] == "#" | sequence[1:series] == "?") &&
-			   (length(sequence) == series || ! '#' %in% sequence[(series + 1):(length(sequence))])) {
-				# We meet all conditions, so we increment the counter
-				return_value <- return_value + 1
-			}
-
-			# Then we test the next iteration, unless we would skip a '#' symbol
-			if(sequence[1] == "#") return(return_value)
-			sequence <- sequence[-1]
-		}
-
-		return(return_value)
+	if(length(series) == 1 && !any(hpos[(index + series):length(hpos)])) {
+		# We meet all conditions, so we have found a possible match
+		return(1)
 	} else {
-		# We increment the starting position as long as we do not leave a '#' behind or
-		# the series is longer than the remaining sequence
-		series_length <- sum(series) + length(series) - 1
-		while(series_length <= length(sequence)) {
-			# The series number eats up as many elements as the number + 1 (for the dot)
-			if(sequence[series[1] + 1] != "#" && all(sequence[1:series[1]] == "#" | sequence[1:series[1]] == "?")) {
-				return_value <- return_value + test_sequence(sequence[-(1:(series[1] + 1))], series[-1])
-			}
+		# The series number eats up as many elements as the number + 1 (for the dot)
+		if(!hpos[index + series[1]]) {
+			# We need to find the next potential indices based on the series
+			# size and the preprocessed sequence
+			next_indices <- which(pp_vect >= series[2])
+			next_indices <- next_indices[next_indices >= (index + series[1] + 1)]
+			remaining_hashes <- which_h[which_h >= (index + series[1] + 1)]
+			next_indices <- next_indices[next_indices <= suppressWarnings(min(remaining_hashes))]
 
-			# Then we try the next position
-			# We cannot eat up a fixed '#' symbol
-			if(sequence[1] == "#") return(return_value)
-			sequence <- sequence[-1]
+			for(i in next_indices) {
+				return_value <- return_value + test_sequence(sequence, seqlength, pp_vect, hpos,
+									     which_h,
+									     index = i,
+									     series[-1])
+			}
 		}
 	}
 
 	return_value
 }
 
-sum(sapply(x, function(x) test_sequence(x[[1]], x[[2]])))
+prepare_indices <- function() {
+
+}
+
+# A function that determines the indices to test and adds them all to get the result
+test_sequence_wrapper <- function(sequence, seqlength, pp_vect, hpos, index, series) {
+	result <- 0
+
+	which_h <- which(hpos)
+	indices_to_test <- which(pp_vect >= series[1])
+	indices_to_test <- indices_to_test[indices_to_test <= min(which_h)]
+
+	for(i in indices_to_test) {
+		result <- result + test_sequence(sequence, seqlength, pp_vect, hpos, which_h, i, series)
+	}
+
+	result
+}
+
+sum(sapply(x, function(x) test_sequence_wrapper(sequence = x[[1]],
+						seqlength = length(x[[1]]),
+						pp_vect = preprocess_sequence(x[[1]]),
+						hpos = hash_positions(x[[1]]),
+						index = 1,
+						series = x[[2]])))
+
 library(microbenchmark)
-microbenchmark(sum(sapply(x, function(x) test_sequence(x[[1]], x[[2]]))))
+microbenchmark(sum(sapply(x, function(x) test_sequence_wrapper(sequence = x[[1]],
+							       seqlength = length(x[[1]]),
+							       pp_vect = preprocess_sequence(x[[1]]),
+							       hpos = hash_positions(x[[1]]),
+							       index = 1,
+							       series = x[[2]]))),
+	       times = 50)
+
+test_sequence_wrapper(sequence = x[[1]][[1]],
+		      seqlength = length(x[[1]][[1]]),
+		      pp_vect = preprocess_sequence(x[[1]][[1]]),
+		      hpos = hash_positions(x[[1]][[1]]),
+		      index = 1,
+		      series = x[[1]][[2]])
 
 # Applying our new function to the modified input
 n_possibilities <- parallel::mclapply(1:length(x), function(i, x) {
 			   message("Processing record ", i, " out of ", length(x))
-			   output <- test_sequence(x[[i]][[1]], x[[i]][[2]])
+			   output <- test_sequence_wrapper(sequence = x[[i]][[1]],
+							   seqlength = length(x[[i]][[1]]),
+							   pp_vect = preprocess_sequence(x[[i]][[1]]),
+							   hpos = hash_positions(x[[i]][[1]]),
+							   index = 1,
+							   series = x[[i]][[2]])
 			   output
-}, x = x, mc.cores = 8)
-
-# Applying our new function to the modified input
-n_possibilities <- parallel::mclapply(1:length(x), function(i, x) {
-			   message("Processing record ", i, " out of ", length(x))
-			   output <- test_sequence(x[[i]][[1]], x[[i]][[2]])
-			   output
-}, x = x, mc.cores = 8)
+	       }, x = x, mc.cores = 8)
 
 # Producing the duplicated records
 x <- lapply(x, function(x) {
@@ -128,5 +171,15 @@ x <- lapply(x, function(x) {
 	       x
 })
 
+n_possibilities <- parallel::mclapply(1:length(x), function(i, x) {
+			   message("Processing record ", i, " out of ", length(x))
+			   output <- test_sequence(sequence = x[[i]][[1]],
+						   seqlength = length(x[[i]][[1]]),
+						   pp_vect = preprocess_sequence(x[[i]][[1]]),
+						   hpos = hash_positions(x[[i]][[1]]),
+						   index = 1,
+						   series = x[[i]][[2]])
+			   output
+	       }, x = x, mc.cores = 8)
 
 cat("Puzzle 12B:", sum(unlist(n_possibilities)), "\n")
